d555bac17c7f23f502fcfcca197d6ce8ff8db5c4
yandi <yandi.cordova87@gmail.com>
2023-11-05 00:32:48 +0100

    probando libreria

--- src/es/yan/Persona.groovy
+++ src/es/yan/Persona.groovy
@@ -4,6 +4,11 @@ abstract class Persona{
     String nombre
     String appelidos
 
+    Persona(String nombre, String apellidos){
+        this.nombre = nombre
+        this.apellidos = apellidos
+    }
+
     void imprimirNombreRojo(){
         echo "\033[31m${nombre}\033[0m"
     }
