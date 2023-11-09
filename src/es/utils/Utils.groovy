1b375e644c8d7010b470fa6a59c1f3bd2d63abce
yandi <yandi.cordova87@gmail.com>
2023-11-09 01:09:13 +0100

    probando libreria

--- src/es/utils/Utils.groovy
+++ src/es/utils/Utils.groovy
@@ -3,7 +3,7 @@ package yan.utils
 class Utils{
     
     static printStaticMethod(){
-        println("Soy un método estático")
+        println("Soy un método estático impreso desde la clase Utils")
         return "Soy un método estático"
     }
 }
